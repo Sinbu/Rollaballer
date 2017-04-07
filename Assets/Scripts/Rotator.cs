6c86bb6f4c5174d87969962d02e040f4fe685f91
Sky Malice <skyleram@gmail.com>
2017-04-06 21:43:29 -0700

    Formatted all scripts, minor cleanup, switched from SetActive(False) to Destory(gameObject).

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour {
-
-	// Update is called once per frame
-	void Update () {
-		this.transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
-	}
+    void Update() {
+        this.transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
 }
--- Assets/Scripts/VerticalMover.cs
+++ Assets/Scripts/VerticalMover.cs
@@ -3,30 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VerticalMover : MonoBehaviour {
+    private bool goingUp;
+    public float topValue;
+    public float bottomValue;
+    public float speed;
 
-	private bool goingUp;
-	public float topValue;
-	public float bottomValue;
-	public float speed;
+    void Start() {
+        goingUp = true;
+    }
 
-	// Use this for initialization
-	void Start () {
-		goingUp = true;
-	}
-
-	// Update is called once per frame
-	void Update () {
-		if (this.transform.position.y >= topValue) {
-			goingUp = false;
-		}
-		if (this.transform.position.y <= bottomValue) {
-			goingUp = true;
-		}
-		if (goingUp == true) {
-			this.transform.Translate (0, speed * Time.deltaTime, 0, Space.World);
-		}
-		if (goingUp == false) {
-			this.transform.Translate (0, -speed * Time.deltaTime, 0, Space.World);
-		}
-	}
+    void Update() {
+        if (this.transform.position.y >= topValue) {
+            goingUp = false;
+        }
+        if (this.transform.position.y <= bottomValue) {
+            goingUp = true;
+        }
+        if (goingUp == true) {
+            this.transform.Translate(0, speed * Time.deltaTime, 0, Space.World);
+        }
+        if (goingUp == false) {
+            this.transform.Translate(0, -speed * Time.deltaTime, 0, Space.World);
+        }
+    }
 }

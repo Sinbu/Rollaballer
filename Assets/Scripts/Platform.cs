75805482e8a9478051248f929075ed29ebe43706
skaugust <skyleram@gmail.com>
2017-04-16 22:17:00 -0700

    Removed tabs from Platform.cs

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -6,8 +6,8 @@ public class Platform : MonoBehaviour {
     public int number;
     public bool passed = false;
     public GameObject startingPoint;
-	
-	void Start () {
-        General.Instance.RegisterPlatform(this);		
-	}
+
+    void Start() {
+        General.Instance.RegisterPlatform(this);
+    }
 }

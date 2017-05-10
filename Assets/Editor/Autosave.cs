a4f5a963f00bbede99be6bb8c57d75b31c3eb875
Sina Yeganeh <sinayega@gmail.com>
2017-05-10 10:37:11 -0700

    Autosave on play button script added

---
+++ Assets/Editor/Autosave.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class AutosaveOnRun {
+    static AutosaveOnRun() {
+        EditorApplication.playmodeStateChanged = () => {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying) {
+                var activeScene = UnityEditor.SceneManagement.EditorSceneManager.GetActiveScene();
+                Debug.Log("Auto-Saving scene before entering Play mode: " + activeScene);
+
+                UnityEditor.SceneManagement.EditorSceneManager.SaveScene(activeScene);
+                AssetDatabase.SaveAssets();
+
+            }
+        };
+    }
+}
